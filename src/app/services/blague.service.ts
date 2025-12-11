99d2d93e1d8636b06e63af846fbfb1d8708dcd48
fatou dieng <dieng.fasou@gmail.com>
2025-12-11 18:02:32 +0100

    affichage pour gitub page

--- src/app/services/blague.service.ts
+++ src/app/services/blague.service.ts
@@ -1,35 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-export interface Blague{
+export interface Blague {
   id: number;
   question: string;
   reponse: string;
-
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlagueService {
- private apiUrl = 'http://localhost:8080/api/blagues';
+
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
-getRandom(): Observable<Blague>{
-  return this.http.get<Blague>(`${this.apiUrl}/random`);
-}
+  getRandom(): Observable<Blague> {
+    return this.http.get<Blague>(`${this.apiUrl}/random`);
+  }
 
-getById(id: number): Observable<Blague>{
-  return this.http.get<Blague>(`${this.apiUrl}/${id}`);
-}
+  getById(id: number): Observable<Blague> {
+    return this.http.get<Blague>(`${this.apiUrl}/${id}`);
+  }
 
-getAll(): Observable<Blague[]>{
-  return this.http.get<Blague[]>(this.apiUrl);
-}
+  getAll(): Observable<Blague[]> {
+    return this.http.get<Blague[]>(this.apiUrl);
+  }
 
-addBlague(blagues:any): Observable<Blague>{
-return this.http.post<Blague>(this.apiUrl, blagues);
-}
+  addBlague(blague: any): Observable<Blague> {
+    return this.http.post<Blague>(this.apiUrl, blague);
+  }
 }
